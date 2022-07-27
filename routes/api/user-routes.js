3eafd6d80451313a040340855cb8ec0964ceb0d0
Robert Routhier <robrouth92@gmail.com>
2022-07-26 18:28:11 -0700

    before school

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,132 +4,151 @@ const {User} = require("../../models")
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
 router.get('/', (req,res)=> {
-    User.findAll({
-        include: [{model: User, as: "friends"}]
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    try {
+        User.findAll({
+            include: [{
+                model: User,
+                as: "friends"
+            }]
+        })
+        .then(users => {
+            res.status(200).json(users)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        }
+        )
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
     }
-    );
 
 })
 
 //TODO - ROUTE THAT CREATES A NEW USER
 router.post('/', (req,res)=> {
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+   try {
+       User.create(req.body)
+       .then(user => {
+           res.status(201).json(user)
+       }).catch(err => {
+           console.log(err)
+           res.status(500).json(err)
+       }
+       )
+   }
+    catch (err) {
+        console.log(err)
+        res.status(500).json(err)
     }
-    );
 
 
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
 router.get('/:userId', (req,res) => {
-    User.findOne({
-        where: {
-            id: req.params.userId
-        },
-        include: [{model: User, as: "friends"}]
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    try {
+        User.findById(req.params.userId, {
+            include: [{
+                model: User,
+                as: "friends"
+            }]
+        })
+        .then(user => {
+            res.status(200).json(user)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        }
+        )
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
     }
-    );
 
 
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
 router.put('/:userId', (req,res)=> {
-    User.update(
-        {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        },
-        {
-            where: {
-                id: req.params.userId
-            }
-        }
-    )
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+   try {
+       User.update(req.body, {
+           where: {
+               id: req.params.userId
+           }
+       })
+       .then(user => {
+           res.status(200).json(user)
+       }).catch(err => {
+           console.log(err)
+           res.status(500).json(err)
+       }
+       )
+   }
+    catch (err) {
+        console.log(err)
+        res.status(500).json(err)
     }
-    );
 
 })
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
 router.delete('/:userId', (req,res)=> {
-    User.destroy({
-        where: {
-            id: req.params.userId
+    try {
+        User.destroy({
+            where: {
+                id: req.params.userId
+            }
+        })
+        .then(user => {
+            res.status(200).json(user)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json(err)
         }
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+        )
     }
-    );
+        catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
 
 
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
 router.put('/:userId/friends/:friendId', (req,res)=> {
-    User.update(
-        {
-            friends: req.body.friends
-        },
-        {
-            where: {
-                id: req.params.userId
-            }
+    try {
+        User.findById(req.params.userId)
+        .then(user => {
+            user.addFriend(req.params.friendId)
+            .then(() => res.json(user))
         }
-    )
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+        )
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
-    );
 
 
 })
 
 //TODO - ROUTE THAT DELETES A FRIEND FROM A USER'S FRIENDS, DONT DELETE THE FRIEND AS A USER THOUGH!
 router.delete('/:userId/friends/:friendId', (req,res)=> {
-    User.destroy({
-        friends: {
-            id: req.params.friendId
-
-        },
-        where: {
-            id: req.params.userId
+    try {
+        User.findById(req.params.userId)
+        .then(user => {
+            user.removeFriend(req.params.friendId)
+            .then(() => res.json(user))
         }
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+        )
+        console.log('SUCESSFULLY DELETED FRIEND')
+        res.status(200).json('SUCESSFULLY DELETED FRIEND')
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
-    );
+
   
 });
 
